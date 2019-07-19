55313c67ee622a43b8b1f37429408477e5e3d311
John Dave Decano <johndavedecano@gmail.com>
2019-07-20 04:05:33 +0800

    fix lint issues

--- src/modules/todos.js
+++ src/modules/todos.js
@@ -1,11 +1,11 @@
 const reducer = {
-    namespaced: true,
-    state: {
-        items: [],
-    },
-    actions: {},
-    mutations: {},
-    getters: {},
+  namespaced: true,
+  state: {
+    items: [],
+  },
+  actions: {},
+  mutations: {},
+  getters: {},
 }
 
 export default reducer
